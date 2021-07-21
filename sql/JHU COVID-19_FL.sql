130ea9a171c2108a03984bc1ebe952efd8e42f22
guarav <guaravrupnar.us@gmail.com>
2021-07-20 18:13:40 -0700

    adding abosoluate path

--- sql/JHU COVID-19_FL.sql
+++ sql/JHU COVID-19_FL.sql
@@ -6,7 +6,7 @@ SET spark.databricks.cloudFiles.schemaInference.sampleSize.numFiles = 2
 CREATE INCREMENTAL LIVE TABLE jhu_covid19_raw
 COMMENT "The raw Johns Hopkins COVID-19 dataset, ingested from /databricks-datasets."
 TBLPROPERTIES ("quality" = "bronze")
-AS SELECT *, input_file_name() AS source_file_name  FROM json.`dbfs:/gaurav/datasets/covid/CSSEGISandData/json/`
+AS SELECT *, input_file_name() AS source_file_name  FROM json.`/dbfs/gaurav/datasets/covid/CSSEGISandData/json/`
 
 -- COMMAND ----------
 
